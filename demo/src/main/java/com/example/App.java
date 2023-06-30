de78b32c6ab799fa607f4c7a5a7323908716209f
Bohdan Liubar <juhim.fed@gmail.com>
2023-06-30 05:54:26 +0300

    Initial commit

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class App {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Addition: " + add(5, 3));
+        System.out.println("Subtraction: " + subtract(5, 3));
+        System.out.println("Multiplication: " + multiply(5, 3));
+        System.out.println("Division: " + divide(5, 3));
+    }
+}
---
+++ demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(8, App.add(5, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, App.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, App.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5.0 / 3, App.divide(5, 3), 0.0001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        App.divide(5, 0);
+    }
